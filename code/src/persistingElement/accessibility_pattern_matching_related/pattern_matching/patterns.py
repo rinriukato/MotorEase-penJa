f7f63dbf11bd8caa40d34cc866829f6b84aa3981
arunkv1 <32751223+arunkv1@users.noreply.github.com>
2023-01-18 21:11:18 -0500

    Add files via upload

---
+++ code/src/persistingElement/accessibility_pattern_matching_related/pattern_matching/patterns.py
@@ -0,0 +1,9 @@
+pattern1 = [
+    {"LOWER": {"IN": ["close", "cancel", "dismiss"]}}
+]
+
+pattern2 = [
+    {"LOWER": {"IN": ["done", "ok", "finish", "return"]}}
+]
+
+# patterns_type1 = [pattern1, pattern2]
