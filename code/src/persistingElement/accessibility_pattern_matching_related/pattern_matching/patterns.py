d4f1615884dcaa048101734773725a5d4c9cefba
arunkv1 <32751223+arunkv1@users.noreply.github.com>
2023-02-21 21:12:25 -0500

    Delete code directory

--- code/src/persistingElement/accessibility_pattern_matching_related/pattern_matching/patterns.py
+++
@@ -1,9 +0,0 @@
-pattern1 = [
-    {"LOWER": {"IN": ["close", "cancel", "dismiss"]}}
-]
-
-pattern2 = [
-    {"LOWER": {"IN": ["done", "ok", "finish", "return"]}}
-]
-
-# patterns_type1 = [pattern1, pattern2]
